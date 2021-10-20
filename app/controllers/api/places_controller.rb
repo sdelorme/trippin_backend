cd1ec87afa29ef5ac9b32fa0b4d5e09f74ed7943
Seth Delorme <delorme.s.m@gmail.com>
2021-10-19 22:42:05 -0400

    changes radius param in api pull to rankby distance param

--- app/controllers/api/places_controller.rb
+++ app/controllers/api/places_controller.rb
@@ -1,6 +1,6 @@
 class Api::PlacesController < ApplicationController
   def nearby_search
-    response = HTTP.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?keyword=#{params[:keyword]}&location=#{params[:lat]},#{params[:lng]}&radius=#{params[:radius]}&type=#{params[:type]}&key=#{Rails.application.credentials.google_maps_api[:api_key]}")
+    response = HTTP.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?keyword=#{params[:keyword]}&location=#{params[:lat]},#{params[:lng]}&rankby=distance&type=#{params[:type]}&key=#{Rails.application.credentials.google_maps_api[:api_key]}")
     
     @places = response.parse
     # render json: @places

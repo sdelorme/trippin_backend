f38133e1002fb10f0c3cbf3886a8a24cf95f2ade
Seth Delorme <delorme.s.m@gmail.com>
2021-10-19 22:34:04 -0400

    adds place view partial and index view for places controller with what I need pulled from nearbysearch api, renders index.json.jb in places nearby search

--- app/controllers/api/places_controller.rb
+++ app/controllers/api/places_controller.rb
@@ -1,6 +1,9 @@
 class Api::PlacesController < ApplicationController
   def nearby_search
     response = HTTP.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?keyword=#{params[:keyword]}&location=#{params[:lat]},#{params[:lng]}&radius=#{params[:radius]}&type=#{params[:type]}&key=#{Rails.application.credentials.google_maps_api[:api_key]}")
-    render json: response.parse
+    
+    @places = response.parse
+    # render json: @places
+    render 'index.json.jb'
   end
 end

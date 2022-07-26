9fb67bc44da964cbbc0d852286ab221b84cbfba0
Seth Delorme <delorme.s.m@gmail.com>
2022-07-25 20:53:17 -0400

    alter one migration file to prepare for production, error in renaming coluimn caused error

--- db/migrate/20220221205107_change_saved_trip_table_to_have_trip_id_column_to_array_of_objects_with_tri_p_events_name.rb
+++ db/migrate/20220221205107_change_saved_trip_table_to_have_trip_id_column_to_array_of_objects_with_tri_p_events_name.rb
@@ -1,6 +1,6 @@
 class ChangeSavedTripTableToHaveTripIdColumnToArrayOfObjectsWithTriPEventsName < ActiveRecord::Migration[6.1]
   def change
-    rename_column :saved_trips, :trip_id, :trip_events
+    rename_column :saved_trips, :trip_event_id, :trip_events
     change_column :saved_trips, :trip_events, :text, default: ""
   end
 end
